56c37e41d28dcfc6f9f5369ff2f4c27cb36bd086
Vladyslav Spivakov <funnydeiv@gmail.com>
2020-01-07 15:31:03 +0000

    jopa

--- js/index.js
+++ js/index.js
@@ -1,14 +1,31 @@
 let i = 0;
+let inp = "";
 const API = "a43a6ae7c50303f26096dc3753718fac";
 let city = "Lviv";
 let status = "";
 let temp = 000;
+let cities = [];
+let errC = 0;
+let errM = "";
 
 document.addEventListener("DOMContentLoaded", init());
 
+document.querySelector("#find-icon").addEventListener("click", checkInput);
+
 function init(){
-    getWeather("Lviv");
-    getWeather("London")
+    
+}
+
+function checkInput(){
+    inp = document.querySelector("header .find-div input").value;
+    if  (inp === "") {
+        document.querySelector("header .find-div input").setAttribute('placeholder','ENTER CITY!!!!!!!!')
+        createErrorToast("404", "You didn`t enter a city name!")
+    } else {
+        document.querySelector("header .find-div input").setAttribute('placeholder','Enter city')
+        getWeather(inp);
+        document.querySelector("header .find-div input").value = "";
+    }
 }
 
 function createToast() {
@@ -19,6 +36,23 @@ function createToast() {
     document.querySelector('.pop-ups').appendChild(newPopUp);
     $('.pop-up[id=pop-up'+i+'] .toast').toast('show');
     i++;
+    checkMaxToasts();
+}
+function createErrorToast(errCode, errMessage) {
+    let newPopUp = document.createElement("div");
+    newPopUp.className = "pop-up";
+    newPopUp.id = "pop-up" + i;
+    newPopUp.innerHTML = '<div class="toast" data-autohide="false" role="alert" aria-live="assertive" aria-atomic="true"> <div class="toast-header"> <img src="https://image.flaticon.com/icons/svg/1199/1199558.svg" class="toast-ico rounded mr-2" alt="..."> <strong class="mr-auto"> Error!</strong> <small>error</small> <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close"> <span aria-hidden="true">&times;</span> </button> </div> <div class="toast-body"> Sorry, something went wrong. Error code: "'+errCode+'". Message: "'+errMessage+'"</div> </div></div>'
+    document.querySelector('.pop-ups').appendChild(newPopUp);
+    $('.pop-up[id=pop-up'+i+'] .toast').toast('show');
+    i++;
+    checkMaxToasts();
+}
+
+function checkMaxToasts() {
+    if (i>5){
+        $('.pop-up[id=pop-up'+(i-6)+'] .toast').toast('dispose');
+    }
 }
 
 function getWeather(c){
@@ -29,11 +63,17 @@ function getWeather(c){
         .then(response => response.json())
         .then(content =>{
             console.log(content);
+            errC=content.cod;
+            errM=content.message;
             temp= content.list[0].main.temp - 273.15;
+            temp = Math.floor(temp * 10) / 10;
             console.log(temp);
             createToast();
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            console.log(err)
+            createErrorToast(errC, errM);
+        });
 
     
     
